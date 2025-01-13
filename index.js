7eff025e14a702e4fb6831037754fde69595383a
ianibsm <ian@ibsm.ca>
2025-01-12 21:07:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/test', (req, res) => {
   res.send('Test route is working!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
